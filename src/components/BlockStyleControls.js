85f8608975df20377ca32cd8ecc06c0e1e8a1163
Ned-el-ch <nedelchev.niki@gmail.com>
2020-01-07 15:20:13 +0000

    restructuring project

---
+++ src/components/BlockStyleControls.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import StyleButton from './StyleButton';
+
+const BlockStyleControls = (props) => {
+	const BLOCK_TYPES = [
+		// {label: 'H1', style: 'header-one'},
+		{label: 'Heading', style: 'header-two'},
+		// {label: 'Sub-heading', style: 'header-three'},
+		{label: 'Sub-heading', style: 'header-four'},
+		// {label: 'H5', style: 'header-five'},
+		// {label: 'H6', style: 'header-six'},
+		{label: 'Blockquote', style: 'blockquote'},
+		{label: 'Bullet List', style: 'unordered-list-item'},
+		{label: 'Numbered List', style: 'ordered-list-item'},
+		{label: 'JS Code Block', style: 'code-block'},
+	];
+	const {editorState} = props;
+	const selection = editorState.getSelection();
+	const blockType = editorState
+		.getCurrentContent()
+		.getBlockForKey(selection.getStartKey())
+		.getType();
+	return (
+		<div className="RichEditor-controls">
+			{BLOCK_TYPES.map((type) =>
+				<StyleButton
+					key={type.label}
+					active={type.style === blockType}
+					label={type.label}
+					onToggle={props.onToggle}
+					style={type.style}
+				/>
+			)}
+		</div>
+	);
+};
+
+export default BlockStyleControls;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import PrismEditor from './PrismEditor';
 
 export default class App extends Component {
