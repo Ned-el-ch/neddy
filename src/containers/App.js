793c1efa91948e8161d7d3962de081388e93b970
Ned-el-ch <nedelchev.niki@gmail.com>
2020-01-08 16:16:45 +0000

    added react router and links

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PrismEditor from './PrismEditor';
 import SignUpPage from './SignUpPage';
 import LoginPage from './LoginPage';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 export default class App extends Component {
 
@@ -24,13 +25,20 @@ export default class App extends Component {
 		return (
 
 			<div className='app'>
+				<Router>
+					<Link to="/" exact>Home</Link><br/>
+					<Link to="/login" exact>Login Page</Link><br/>
+					<Link to="/signup" exact>Signup Page</Link><br/>
+					<Link to="/editor" exact>Editor Page</Link><br/>
 
-				{/* < SignUpPage /> */}
-				< LoginPage handleLogin={this.handleLogin}/>
-				< PrismEditor />
-	
+					<Route exact path='/signup' component={SignUpPage} />
+					<Route exact path='/login' component={LoginPage} />
+					<Route exact path='/editor' component={PrismEditor} />
+
+				</Router>
+
 			</div>
-	
+
 		);
 
 	};
